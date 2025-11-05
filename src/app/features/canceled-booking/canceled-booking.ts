414376f30e3279383b61156268e7ba3995a912b6
MF <mf01000009120@gmail.com>
2025-11-05 22:10:57 +0200

    FinishBooking

---
+++ src/app/features/canceled-booking/canceled-booking.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-canceled-booking',
+  imports: [],
+  templateUrl: './canceled-booking.html',
+  styleUrl: './canceled-booking.scss',
+})
+export class CanceledBooking {
+
+}
---
+++ src/app/features/completed-booking/completed-booking.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-completed-booking',
+  imports: [],
+  templateUrl: './completed-booking.html',
+  styleUrl: './completed-booking.scss',
+})
+export class CompletedBooking {
+
+}
