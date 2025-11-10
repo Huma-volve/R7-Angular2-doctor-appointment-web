b7a0a926be079c2294cf2a7f9be6a92ceabbc8f7
MF <mf01000009120@gmail.com>
2025-11-10 21:26:33 +0200

    Edit interceptors

--- src/app/core/interceptors/global.ts
+++ src/app/core/interceptors/global.ts
@@ -1,8 +1,30 @@
-import { Injectable } from '@angular/core';
+import { HttpInterceptorFn } from '@angular/common/http';
+import { finalize } from 'rxjs';
+import { NgxSpinnerService } from 'ngx-spinner';
+import { inject, PLATFORM_ID } from '@angular/core';
+import { environment } from '../environment/environment';
+import { isPlatformBrowser } from '@angular/common';
 
-@Injectable({
-  providedIn: 'root',
-})
-export class Global {
-  
-}
+export const globalInterceptor: HttpInterceptorFn = (req, next) => {
+  const id = inject(PLATFORM_ID);
+  const spinner = inject(NgxSpinnerService);
+  let userToken = '';
+  if (isPlatformBrowser(id)) {
+    userToken = localStorage.getItem('userToken') || '';
+  }
+
+  spinner.show();
+
+  if (req.url.includes('/assets/')) {
+    return next(req).pipe(finalize(() => spinner.hide()));
+  }
+
+  const myReq = req.clone({
+    url: environment.apiBaseUrl + req.url,
+    setHeaders: {
+      Authorization: userToken ? `Bearer ${userToken}` : '',
+    },
+  });
+
+  return next(myReq).pipe(finalize(() => spinner.hide()));
+};
