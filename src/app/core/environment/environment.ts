69ac3b8f98a0195fbe66cc94478636e0b251046c
davidzakii <davidzaki77@gmail.com>
2025-11-10 22:20:37 +0200

    feat: edit navbar style and add payment UI

--- src/app/core/environment/environment.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  apiBaseUrl: '',
-  socketUrl: '', // Socket.IO endpoint
-};
--- src/app/shared/components/navbar/navbar.ts
+++ src/app/shared/components/navbar/navbar.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './navbar.html',
   styleUrl: './navbar.scss',
 })
 export class Navbar {
-
+  toggle: boolean = false;
+  changeToggle() {
+    this.toggle = !this.toggle;
+  }
 }
