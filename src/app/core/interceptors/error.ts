555c8215ac48a43c32bee3299be3247315f52db8
MF <mf01000009120@gmail.com>
2025-11-10 21:26:33 +0200

    Edit interceptors

--- src/app/core/interceptors/error.ts
+++ src/app/core/interceptors/error.ts
@@ -1,8 +1,20 @@
-import { Injectable } from '@angular/core';
+import { HttpInterceptorFn, HttpErrorResponse } from '@angular/common/http';
+import { catchError, throwError } from 'rxjs';
+import { inject } from '@angular/core';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 
-@Injectable({
-  providedIn: 'root',
-})
-export class Error {
-  
-}
+export const errorInterceptor: HttpInterceptorFn = (req, next) => {
+  const toastr = inject(ToastrService);
+  const router = inject(Router);
+  return next(req).pipe(
+    catchError((error: HttpErrorResponse) => {
+      toastr.error(error.error?.message || 'Unexpected error', `Error ${error.status}`);
+      if (error.status === 401) {
+        router.navigate(['/auth/login']);
+      }
+
+      return throwError(() => error);
+    })
+  );
+};
