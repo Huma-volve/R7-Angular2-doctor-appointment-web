27079f5df8b2327e99d6e35c5182b38de63e6bd5
mennafared <mennafarid700@gmail.com>
2025-11-15 08:17:21 +0200

    finish appointment and map pages

--- src/app/features/doctors-list/doctors-list.ts
+++ src/app/features/doctors-list/doctors-list.ts
@@ -1,44 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, inject, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Router, RouterLink  } from '@angular/router';
-
-
-interface Doctor {
-  id: number;
-  name: string;
-  specialty: string;
-  hospital: string;
-  rating: number;
-  availableTime: string;
-  price: number;
-  gender: 'Male' | 'Female';
-  image: string;
-}
+import { Doctor } from '../interfaces/doctor';
+import { Doctors } from '../Services/doctors';
 
 @Component({
   selector: 'app-doctors-list',
   standalone: true,
-  imports: [CommonModule, FormsModule , RouterLink],
+  imports: [CommonModule, FormsModule , RouterLink ],
   templateUrl: './doctors-list.html',
   styleUrls: ['./doctors-list.scss'],
 })
-export class DoctorsList implements OnInit {
+export class DoctorsList {
+  private readonly doctorService = inject(Doctors);
+
+
+  constructor(private router: Router) { }
 
   isSidebarCollapsed: boolean = true;
 
+  allDoctors = signal<Doctor[]>([])
 
-  availableDateFilter: 'Today' | 'Tomorrow' | null = null;
-  genderFilter: 'Male' | 'Female' | null = null;
-  consultationTypeFilter: 'In-clinic' | 'Home Visit' | null = null;
-  sortFilter: 'recommended' | 'priceLowToHigh' | 'priceHighToLow' = 'recommended';
+  sortFilter: string = '';
   searchText: string = '';
   selectedSpecialties: string[] = [];
-
-
-  allDoctors: Doctor[] = [];
-  filteredDoctors: Doctor[] = [];
-
 
   specialties = [
     { name: 'Dentist', icon: 'fa-tooth' },
@@ -57,11 +43,41 @@ export class DoctorsList implements OnInit {
     { label: 'Price High to Low', value: 'priceHighToLow' },
   ];
 
-  ngOnInit(): void {
-    this.allDoctors = this.getMockDoctors();
-    this.applyFilters();
-  }
+  applyFilters(): void {
+    let tempDoctors = [...this.allDoctors()];
 
+    // Search Text Filter
+    if (this.searchText) {
+      const lowerCaseSearch = this.searchText.toLowerCase();
+      tempDoctors = tempDoctors.filter(doc =>
+        doc.fullName.toLowerCase().includes(lowerCaseSearch) ||
+        doc.specialistTitle.toLowerCase().includes(lowerCaseSearch)
+      );
+    }
+
+    // Specialty Filter
+    if (this.selectedSpecialties.length > 0) {
+      tempDoctors = tempDoctors.filter(doc =>
+        this.selectedSpecialties.includes(doc.specialistTitle)
+      );
+    }
+
+    // Sort Filter
+    switch (this.sortFilter) {
+      case 'priceLowToHigh':
+        tempDoctors.sort((a, b) => a.price - b.price);
+        break;
+      case 'priceHighToLow':
+        tempDoctors.sort((a, b) => b.price - a.price);
+        break;
+      case 'recommended':
+      default:
+        tempDoctors.sort((a, b) => b.rating - a.rating);
+        break;
+    }
+
+    this.allDoctors.set(tempDoctors) ;
+  }
 
   toggleSidebar(): void {
     this.isSidebarCollapsed = !this.isSidebarCollapsed;
@@ -82,68 +98,20 @@ export class DoctorsList implements OnInit {
     return this.selectedSpecialties.includes(specialtyName);
   }
 
-
-  applyFilters(): void {
-    let tempDoctors = [...this.allDoctors];
-
-    // Search Text Filter
-    if (this.searchText) {
-      const lowerCaseSearch = this.searchText.toLowerCase();
-      tempDoctors = tempDoctors.filter(doc =>
-        doc.name.toLowerCase().includes(lowerCaseSearch) ||
-        doc.specialty.toLowerCase().includes(lowerCaseSearch) ||
-        doc.hospital.toLowerCase().includes(lowerCaseSearch)
-      );
-    }
-
-    // Specialty Filter
-    if (this.selectedSpecialties.length > 0) {
-      tempDoctors = tempDoctors.filter(doc =>
-        this.selectedSpecialties.includes(doc.specialty)
-      );
-    }
-
-    // Gender Filter
-    if (this.genderFilter) {
-      tempDoctors = tempDoctors.filter(doc => doc.gender === this.genderFilter);
-    }
-
-    // Sort Filter
-    switch (this.sortFilter) {
-      case 'priceLowToHigh':
-        tempDoctors.sort((a, b) => a.price - b.price);
-        break;
-      case 'priceHighToLow':
-        tempDoctors.sort((a, b) => b.price - a.price);
-        break;
-      case 'recommended':
-      default:
-        tempDoctors.sort((a, b) => b.rating - a.rating); // Higher rating first
-        break;
-    }
-
-    this.filteredDoctors = tempDoctors;
+    private loadDoctors(): void {
+      this.doctorService.getAllDoctors().subscribe({
+      next: (response) => {
+        this.allDoctors.set(response.data);
+          console.log(this.allDoctors);
+      },
+    });
   }
-
-  // Doctors Information
-  private getMockDoctors(): Doctor[] {
-    return [
-      { id: 1, name: 'Robert Johnson', specialty: 'Orthopedic', hospital: 'El-Nasr Hospital', rating: 4.8, availableTime: '9:30am - 8:00pm', price: 350, gender: 'Male', image: 'assets/images/doctor-1.svg' },
-      { id: 2, name: 'Alice Smith', specialty: 'Cardiologist', hospital: 'City General', rating: 4.9, availableTime: '10:00am - 7:00pm', price: 400, gender: 'Female', image: 'assets/images/doctor-2.svg' },
-      { id: 3, name: 'Emily White', specialty: 'Pediatrician', hospital: 'Children\'s Hospital', rating: 4.7, availableTime: '8:00am - 5:00pm', price: 300, gender: 'Female', image: 'assets/images/doctor-3.svg' },
-      { id: 4, name: 'Michael Brown', specialty: 'Dentist', hospital: 'Dental Clinic', rating: 4.5, availableTime: '9:00am - 6:00pm', price: 250, gender: 'Male', image: 'assets/images/doctor-4.svg' },
-      { id: 5, name: 'Sarah Davis', specialty: 'Neurologist', hospital: 'St. Jude Medical', rating: 4.9, availableTime: '11:00am - 8:00pm', price: 450, gender: 'Female', image: 'assets/images/doctor-5.svg' },
-      { id: 6, name: 'David Lee', specialty: 'Orthopedic', hospital: 'El-Nasr Hospital', rating: 4.6, availableTime: '9:00am - 7:00pm', price: 370, gender: 'Male', image: 'assets/images/doctor-6.svg' },
-      { id: 7, name: 'Jessica Chen', specialty: 'Cardiologist', hospital: 'Heart Institute', rating: 4.8, availableTime: '8:30am - 5:30pm', price: 420, gender: 'Female', image: 'assets/images/doctor-7.svg' },
-      { id: 8, name: 'Kevin Wang', specialty: 'Dentist', hospital: 'Happy Teeth Clinic', rating: 4.7, availableTime: '10:00am - 7:00pm', price: 280, gender: 'Male', image: 'assets/images/doctor-8.svg' },
-      { id: 9, name: 'Olivia Kim', specialty: 'Pulmonologist', hospital: 'Lung Health Center', rating: 4.9, availableTime: '9:00am - 6:00pm', price: 380, gender: 'Female', image: 'assets/images/doctor-9.svg' }
-    ];
-  }
-
-  constructor(private router: Router) { }
 
   bookAppointment(doctorId: number): void {
     this.router.navigate(['/layout/appointment', doctorId]);
   }
 
+  ngOnInit(): void {
+    this.loadDoctors();
+  }
 }
