1a59d13fb72f11f06169a3e593386090562c3de3
MF <mf01000009120@gmail.com>
2025-11-18 14:38:17 +0200

    FixNavBar And add Home Search

--- src/app/shared/components/navbar/navbar.ts
+++ src/app/shared/components/navbar/navbar.ts
@@ -1,28 +1,58 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
 import { Router, RouterLink } from '@angular/router';
-import { AuthService } from '../../../auth/service/auth';
-import { ToastrService } from 'ngx-toastr';
+import { CommonModule, SlicePipe } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Doctors } from '../../../features/Services/doctors';
+import { IUserDetails } from '../../../features/interfaces/iuser-details';
+import { environment } from '../../../core/environment/environment';
 
 @Component({
   selector: 'app-navbar',
-  imports: [RouterLink],
+  imports: [RouterLink, SlicePipe, FormsModule, CommonModule],
   templateUrl: './navbar.html',
   styleUrl: './navbar.scss',
 })
-export class Navbar {
-  private authService = inject(AuthService);
-  private toastr = inject(ToastrService);
+export class Navbar implements OnInit {
+  private readonly doctorService = inject(Doctors);
   private router = inject(Router);
+  data = signal<IUserDetails | null>(null);
+  filtered = signal<any[]>([]);
   toggle: boolean = false;
+  baseUrl = environment.apiBaseUrl;
+
   changeToggle() {
     this.toggle = !this.toggle;
   }
-  logout() {
-    this.authService.logout().subscribe({
-      next: (res) => {
-        this.toastr.success(res.message);
-        this.router.navigate(['auth/login']);
+
+  ngOnInit(): void {
+    const saved = localStorage.getItem('userDetails');
+    if (saved) {
+      this.data.set(JSON.parse(saved) as IUserDetails);
+    }
+  }
+  onSearch(doctorName: string): void {
+    const body = {
+      keyword: doctorName,
+      specialityId: null,
+      gender: null,
+      sortBy: null,
+      pageNumber: 1,
+      pageSize: 10,
+    };
+
+    this.doctorService.searchDoctors(body).subscribe({
+      next: (response) => {
+        this.filtered.set(response.data.doctors);
       },
     });
+  }
+  closeSearch(searchInput?: HTMLInputElement) {
+    this.filtered.set([]);
+    if (searchInput) searchInput.value = '';
+  }
+
+  logout() {
+    localStorage.clear();
+    this.router.navigate(['auth/login']);
   }
 }
