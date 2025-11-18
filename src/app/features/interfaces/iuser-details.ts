612c446e25b2ddbaee14d18417964bf0d76a97fb
MF <mf01000009120@gmail.com>
2025-11-18 14:38:17 +0200

    FixNavBar And add Home Search

---
+++ src/app/features/interfaces/iuser-details.ts
@@ -0,0 +1,15 @@
+export interface IUserDetails {
+  sub: string;
+  unique_name: string;
+  firstName: string;
+  lastName: string;
+  address: string;
+  imgUrl: string;
+  birthDate: string;
+  gender: string;
+  location: string;
+  isNotificationsEnabled: string;
+  exp: number;
+  iss: string;
+  aud: string;
+}
